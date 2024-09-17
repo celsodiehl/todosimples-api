4b576ef2794bc5877b054c299b54537f5dea10d8
Celso <celsodiehl@gmail.com>
2024-09-17 09:08:49 -0300

    Create Task model

--- src/main/java/com/celsodiehl/todosimples/models/User.java
+++ src/main/java/com/celsodiehl/todosimples/models/User.java
@@ -1,19 +1,19 @@
 package com.celsodiehl.todosimples.models;
 
-import jakarta.persistence.Access;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-
-import org.springframework.data.web.JsonPath;
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -44,7 +44,8 @@ public class User {
     @Size(groups = CreateUser.class, min = 8, max = 60)
     private String password;
 
-    // private List<Task>
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<>();
 
     public User() {
     }
@@ -77,6 +78,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
