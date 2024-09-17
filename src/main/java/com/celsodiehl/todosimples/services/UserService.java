e104db8e0d6288f5e08cfe7bf046a2800a1b906d
Celso <celsodiehl@gmail.com>
2024-09-17 16:41:37 -0300

    Create User Service

---
+++ src/main/java/com/celsodiehl/todosimples/services/UserService.java
@@ -0,0 +1,51 @@
+package com.celsodiehl.todosimples.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.celsodiehl.todosimples.repositories.UserRepository;
+import com.celsodiehl.todosimples.models.User;
+import com.celsodiehl.todosimples.repositories.TaskRepository;
+
+@Service
+public class UserService {
+
+    // CONSTRUTOR do Service
+    @Autowired
+    private UserRepository uRepository;
+
+    @Autowired
+    private TaskRepository tRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.uRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        this.tRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.uRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try {
+            this.uRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é Possível excluir, possui Relacionamentos!");
+        }
+        
+    }
+}
